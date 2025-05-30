9fcc322d0c3f8086430ec1cb4d71a5abb7d22b52
Juan Carlos Barrera Barrera <51795550+Juancarlos56@users.noreply.github.com>
2025-05-30 10:08:32 -0500

    actualizacion de funcionalidad
    Se agrego un select para identificacion de las plantas al momento de crear un producto

--- frontend/src/app/components/producto/form-productos/form-productos.component.ts
+++ frontend/src/app/components/producto/form-productos/form-productos.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule, NgForm} from '@angular/forms'
 import { Producto } from '../../../models/producto.model';
+import { PlantaService } from '../../../services/planta.service';
+import { Planta } from '../../../models/planta.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-productos',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form-productos.component.html',
   styleUrl: './form-productos.component.css'
 })
-export class FormProductosComponent {
+export class FormProductosComponent  implements OnInit{
 
   @Input() producto: Producto;
   
@@ -17,8 +20,25 @@ export class FormProductosComponent {
 
   @Output() newPlantaEventEmitter: EventEmitter<Producto> = new EventEmitter();
 
-  constructor() {
+  plantas: Planta[] = [];
+
+  constructor(private plantaService: PlantaService) {
     this.producto = new Producto();
+  }
+
+  ngOnInit(): void {
+    this.getPlantas();
+  }
+  
+  getPlantas(){
+    this.plantaService.getAll().subscribe({
+      next: (data: any) => {
+        this.plantas = data;
+      },
+      error: (err: any) => {
+        console.error('Error al obtener plantas:', err);
+      }
+    });
   }
 
   onSubmit(userForm: NgForm): void {
